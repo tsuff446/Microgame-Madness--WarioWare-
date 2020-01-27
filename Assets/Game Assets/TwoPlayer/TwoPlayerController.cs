9ec7cde9d1ab7c8e7b6910eb38136e281674b3d9
Thomas446 <36467286+Thomas446@users.noreply.github.com>
2020-01-27 15:25:51 -0600

    Added 2 Player Test

---
+++ Assets/Game Assets/TwoPlayer/TwoPlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoPlayerController : MonoBehaviour
+{
+    public int playerNum = 1;
+    private Rigidbody2D rb;
+    private Transform tm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        tm = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal_Player" + playerNum) != 0)
+        {
+            rb.AddForce( new Vector2(Input.GetAxis("Horizontal_Player" + playerNum), 0));
+        }
+        if (Input.GetAxis("Vertical_Player" + playerNum) != 0)
+        {
+            rb.AddForce(new Vector2(0, Input.GetAxis("Vertical_Player" + playerNum)));
+        }
+        
+    }
+}
